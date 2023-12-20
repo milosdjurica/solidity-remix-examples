e54f12f9c5ddc454aea64e2b2951b21385ee073e
milosdjurica <milosdjurica13@gmail.com>
2023-12-20 23:00:57 +0100

    added comments

--- delegateCall/Example.sol
+++ delegateCall/Example.sol
@@ -18,6 +18,13 @@ contract A {
     uint256 public num;
     uint256 public value;
 
+    // ! It calls function from contract B and sets values on their storage slots
+    // * num = _num;
+    // * sender = msg.sender;
+    // * value = msg.value;
+    // ! first storage slot in contract A (sender) will get NUM value,
+    // ! 2nd storage slot in contract A (num) -> msg.sender
+    // ! 3rd storage slot in A (value) -> value
     function setVars(address _contract, uint256 _num) public payable {
         (bool success, bytes memory data) = _contract.delegatecall(abi.encodeWithSignature("setVars(uint256)", _num));
     }
