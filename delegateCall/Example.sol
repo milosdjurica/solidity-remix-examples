5d59e51fb6b2e16722df783b4d2d39b6ed30e7a1
milosdjurica <milosdjurica13@gmail.com>
2023-12-20 23:11:00 +0100

    bool and comment

--- delegateCall/Example.sol
+++ delegateCall/Example.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.19;
 
 contract B {
+    // ! Compares this storage slots with those in contract A
     uint256 public num;
     address public sender;
     uint256 public value;
@@ -16,7 +17,7 @@ contract B {
 contract A {
     address public sender;
     uint256 public num;
-    uint256 public value;
+    bool public value;
 
     // ! It calls function from contract B and sets values on their storage slots
     // * num = _num;
@@ -24,7 +25,7 @@ contract A {
     // * value = msg.value;
     // ! first storage slot in contract A (sender) will get NUM value,
     // ! 2nd storage slot in contract A (num) -> msg.sender
-    // ! 3rd storage slot in A (value) -> value
+    // ! 3rd storage slot in A (value) -> value ---> sets to false if value is 0, true in any other case
     function setVars(address _contract, uint256 _num) public payable {
         (bool success, bytes memory data) = _contract.delegatecall(abi.encodeWithSignature("setVars(uint256)", _num));
     }
